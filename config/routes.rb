1cb005b673369eb25bc81e67d087160b3f0355ef
ashley <ashleyot@gmail.com>
2023-01-02 20:18:19 -0500

    autocomplete example working...need to add debounce

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts do
+    collection do
+      post :search
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
